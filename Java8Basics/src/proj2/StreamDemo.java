44cbde95b85707339ab5ca1027df7b7e54b7d374
SarthakPatel86 <sarthakppatel@gmail.com>
2024-11-19 17:32:04 +0530

    map

--- Java8Basics/src/proj2/StreamDemo.java
+++ Java8Basics/src/proj2/StreamDemo.java
@@ -2,6 +2,8 @@ package proj2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamDemo {
@@ -11,5 +13,14 @@ public class StreamDemo {
 
 		List<Integer> l2 = l1.stream().map((n) -> n + 3).collect(Collectors.toList());
 		System.out.println(l2);
+
+		List<Integer> l3 = Arrays.asList(12, 5, 12, 4, 86, 42, 12, 56);
+		Set<Integer> s1 = l3.stream().map((n) -> n + 10).collect(Collectors.toSet());
+		System.out.println(s1);
+		
+		List<String> l4=Arrays.asList("Virat","Abd","Rcb","Csk","Dhoni","Gill");
+		Map<Character, String> m1= l4.stream().map((s)->s.toUpperCase()).collect(Collectors.toMap((s)->s.charAt(0),s->s));
+		System.out.println(m1);
+				
 	}
 }
